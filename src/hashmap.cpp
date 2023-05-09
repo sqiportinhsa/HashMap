970aa203a104bd114c044a5a377405e958146c5c
sqiportinhsa <97836951+sqiportinhsa@users.noreply.github.com>
2023-05-10 01:13:52 +0300

    replace int with hash_t for hash type

--- src/hashmap.cpp
+++ src/hashmap.cpp
@@ -4,7 +4,7 @@
 
 static void init_lists(List *array, size_t size);
 
-HashMap *hashmap_init(int (*hashfunc)(char *key), size_t array_size) {
+HashMap *hashmap_init(hash_t (*hashfunc)(char *key), size_t array_size) {
     HashMap *map = (HashMap*) calloc(1, sizeof(HashMap));
     if (!map) return nullptr;
 
@@ -23,12 +23,12 @@ HashMap *hashmap_init(int (*hashfunc)(char *key), size_t array_size) {
 }
 
 void hashmap_insert(HashMap *hashmap, char *key, char *value) {
-    int hash = hashmap->hashfunc(key) % hashmap->array_size;
+    hash_t hash = hashmap->hashfunc(key) % hashmap->array_size;
     list_insert(hashmap->array + hash, key, value);
 }
 
 char *hashmap_find(HashMap *hashmap, char *key) {
-    int hash = hashmap->hashfunc(key) % hashmap->array_size;
+    hash_t hash = hashmap->hashfunc(key) % hashmap->array_size;
     return list_find(hashmap->array + hash, key);
 }
 
--- src/hashmap.h
+++ src/hashmap.h
@@ -1,13 +1,19 @@
+#ifndef HASHMAP_H
+#define HASHMAP_H
+
 #include "list.h"
+#include "hash.h"
 
 struct HashMap {
-    int (*hashfunc)(char *key);
+    hash_t (*hashfunc)(char *key);
     List *array = nullptr;
     size_t array_size = 0;
     char *input = nullptr;
 };
 
-HashMap *hashmap_init(int (*hashfunc)(char *key), size_t array_size);
-void     hashmap_insert(HashMap *hashmap, char *key, char *value);
-char    *hashmap_find  (HashMap *hashmap, char *key);
+HashMap *hashmap_init  (hash_t (*hashfunc)(char *key), size_t array_size);
+void     hashmap_insert(HashMap *hashmap,  char *key, char *value);
+char    *hashmap_find  (HashMap *hashmap,  char *key);
 void     hashmap_dtor  (HashMap *hashmap);
+
+#endif
