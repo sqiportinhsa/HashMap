c7ab198da674c0eb7abc0fe628d3a43e0b7ace64
sqiportinhsa <97836951+sqiportinhsa@users.noreply.github.com>
2023-05-09 23:24:41 +0300

    add list

---
+++ src/list.cpp
@@ -0,0 +1,51 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "list.h"
+
+static void list_resize(List *list);
+
+void list_init(List *list) {
+    list->max_size = 0;
+    list->size     = 0;
+    list->nodes = nullptr;
+}
+
+void list_dtor(List *list) {
+    free(list->nodes);
+    free(list);
+}
+
+void list_insert(List *list, char *key, char *value) {
+    if (list->size == list->max_size) {
+        list_resize(list);
+        assert(list->nodes);
+    }
+
+    list->nodes[list->size].value = value;
+    strncpy(list->nodes[list->size].key, key, MAX_KEY_SIZE);
+    ++list->size;
+};
+
+char *list_find(List *list, char *key) {
+    char *value = nullptr;
+    for (size_t i = 0; i < list->size; ++i) {
+        if (!strcmp(list->nodes[i].key, key)) {
+            value = list->nodes[i].value;
+            break;
+        }
+    }
+    return value;
+}
+
+static void list_resize(List *list) {
+    if (!list->nodes) {
+        list->nodes = (Node*) calloc(INITIAL_LIST_SIZE, sizeof(Node));
+        list->max_size = INITIAL_LIST_SIZE;
+    }
+
+    list->nodes = (Node*) realloc(list->nodes, list->max_size * 2 * sizeof(Node));
+    list->max_size *= 2;
+}
+
---
+++ src/list.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+const size_t MAX_KEY_SIZE = 32;
+const size_t INITIAL_LIST_SIZE = 1;
+
+struct Node {
+    char key[MAX_KEY_SIZE] = "";
+    char *value = nullptr;
+};
+
+struct List {
+    size_t max_size = 0;
+    size_t     size = 0;
+
+    Node *nodes = nullptr;
+};
+
+void  list_init  (List *list);
+void  list_dtor  (List *list);
+void  list_insert(List *list, char *key, char *value);
+char *list_find  (List *list, char *key);
