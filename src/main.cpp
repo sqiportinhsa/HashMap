5513fdb48ed6a29bcd9ae8f7927942fa3460d5ac
sqiportinhsa <97836951+sqiportinhsa@users.noreply.github.com>
2023-05-13 16:54:50 +0300

    change hashmap size

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,10 @@
 #include "input.h"
 
 const char *INPUT_FILENAME = "../data/big2.txt";
+const size_t HASHMAP_SIZE = 16127;
 
 int main() {
-    HashMap *hashmap = hashmap_init(const_hash, 5);
+    HashMap *hashmap = hashmap_create(const_hash, HASHMAP_SIZE);
     Input input = get_input(INPUT_FILENAME);
     fill_hashmap(input, hashmap);
     printf("%s %s!\n", hashmap_find(hashmap, "Hello"), hashmap_find(hashmap, "World"));
