c7cac8538c430d3f110322f279a066f4380ba818
sqiportinhsa <97836951+sqiportinhsa@users.noreply.github.com>
2023-05-15 14:16:37 +0300

    update tests

--- test/main.cpp
+++ test/main.cpp
@@ -8,7 +8,9 @@ const char *DATA_FILENAME = "../data/big.txt";
 
 int main() {
     Input input = get_input(DATA_FILENAME);
-    check_hashmap(crc32_hash,      input);
+    Pairs pairs = get_pairs(input);
+    qsort(pairs.pairs, pairs.pairs_amount, sizeof(Pair), comparator);
+    check_hashmap(crc32_hash, pairs);
     //check_hashmap(const_hash,      input);
     //check_hashmap(first_char_hash, input);
     //check_hashmap(char_sum_hash,   input);
@@ -16,5 +18,6 @@ int main() {
     //check_hashmap(rol_hash,        input);
     //check_hashmap(gnu_hash,        input);
     free(input.data);
+    free(pairs.pairs);
     return 0;
 }
