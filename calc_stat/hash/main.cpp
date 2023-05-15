0e9e4f13eeabf0151e7e17f91258121712f7be79
sqiportinhsa <97836951+sqiportinhsa@users.noreply.github.com>
2023-05-15 14:19:16 +0300

    calc stat: split to pairs to calc time of hashfuncs work

--- calc_stat/hash/main.cpp
+++ calc_stat/hash/main.cpp
@@ -6,9 +6,11 @@ const char *DATA_FILENAME = "../data/big.txt";
 
 int main() {
     Input input = get_input(DATA_FILENAME);
+    Pairs dict  = get_pairs(input);
     double times[HASHFUNCS_SIZE] = {};
-    get_times(input, times);
+    get_times(times, dict);
     print_times(times);
-    get_and_report_all_distributions(input);
+    get_and_report_all_distributions(dict);
     free(input.data);
+    free(dict.pairs);
 }
