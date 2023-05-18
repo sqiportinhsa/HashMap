70a695f84fbfd9652935c778011c9f92c83a1d4c
sqiportinhsa <97836951+sqiportinhsa@users.noreply.github.com>
2023-05-18 22:55:23 +0300

    v2: asm strcmp

--- src/list.cpp
+++ src/list.cpp
@@ -5,6 +5,7 @@
 #include "list.h"
 
 static void list_resize(List *list);
+static int __attribute__((always_inline)) strcmp_asm(const char *srt1, const char *str2);
 
 void list_init(List *list) {
     list->max_size = 0;
@@ -30,12 +31,35 @@ void list_insert(List *list, const char *key, const char *value) {
 const char *list_find(List *list, const char *key) {
     const char *value = nullptr;
     for (size_t i = 0; i < list->size; ++i) {
-        if (!strcmp(list->nodes[i].key, key)) {
+        if (!strcmp_asm(list->nodes[i].key, key)) {
             value = list->nodes[i].value;
             break;
         }
     }
     return value;
+}
+
+static int __attribute__((always_inline)) strcmp_asm(const char *srt1, const char *str2) {
+    int result = 0;
+
+    asm(
+    ".intel_syntax noprefix\n"
+
+    "vmovdqu ymm0, ymmword ptr [%1]\n" /* ymm0 <- srt1 */
+    "vmovdqu ymm1, ymmword ptr [%2]\n" /* ymm1 <- str2 */
+
+    "vptest  ymm0, ymm1\n"             /* cf = (srt1 == str2) */
+    "mov %0, 0\n"       
+    "setnc %b0\n"
+
+    ".att_syntax prefix\n"
+
+    : "=r"(result)
+    : "r"(srt1), "r"(str2)
+    : "ymm0", "ymm1", "cc"
+    );
+
+    return result;
 }
 
 static void list_resize(List *list) {
