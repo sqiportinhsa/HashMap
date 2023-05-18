42ef841aa477a50e9ababd47d5ece07ac8dcc71f
sqiportinhsa <97836951+sqiportinhsa@users.noreply.github.com>
2023-05-18 18:54:25 +0300

    add benchmark for search

---
+++ calc_stat/search/main.cpp
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+#include "bench.h"
+
+const char *DATA_FILENAME = "../data/big.txt";
+
+int main() {
+    Input input = get_input(DATA_FILENAME);
+    Pairs dict  = get_pairs(input);
+
+    HashMap *hashmap = hashmap_create(crc32_hash, dict.pairs_amount);
+    get_time(hashmap, dict);
+
+    hashmap_dtor(hashmap);
+    free(input.data);
+    free(dict.pairs);
+}
